3e651d6bdbcfb042205880a3eb9d5afc1f9b0668
agorapilled <johngardner13@outlook.com>
2024-11-11 17:37:53 -0500

    v0

--- src/rmarquee.js
+++ src/rmarquee.js
@@ -4,7 +4,7 @@ import Marquee from 'react-fast-marquee';
 const RepeatedMarquee = ({direction}) => {
   // Create an array of 10 elements
   const marqueeText = Array.from({ length: 10 }, (_, index) => (
-    <span key={index} className="font-custom text-4xl md:text-6xl text-blue-600 flex justify-center items-center">
+    <span key={index} className="font-custom text-5xl md:text-6xl text-blue-600 flex justify-center items-center">
       Inverse Cramer ETF <img src="comp.gif" className='w-[100px] ml-2'></img>
     </span>
   ));
